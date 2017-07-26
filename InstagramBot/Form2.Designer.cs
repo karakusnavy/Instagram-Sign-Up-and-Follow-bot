0c89357a69c51235e5ce1ff208776a7bf048d381
karakusnavy <karakusnavy@gmail.com>
2017-07-26 17:40:34 +0300

    Program güncellendi

---
+++ InstagramBot/Form2.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace InstagramBot
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(168, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "İSİM LİSTESİ GİRİNİZ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(168, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TÜMÜNÜ SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(168, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "OLUŞTUR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OLUŞTURULAN İSİN SOYİSİM SAYISI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(214, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "OLUŞTURULAN KULLANICI ADI SAYISI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(222, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 189);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(118, 95);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(211, 189);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(125, 95);
+            this.listBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(133, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 95);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "(.TXT)  ÇIKAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "İsim Soyisimler";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(213, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Kullanıcı Adları";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-106, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 296);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rasgele Kullanıcı Adı ve İsim Soyisim Oluşturma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
---
+++ InstagramBot/Form2.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System.IO;
+
+namespace InstagramBot
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader oku = new StreamReader(openFileDialog1.FileName);
+                string satir = oku.ReadLine();
+                while (satir != null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir = oku.ReadLine();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+        string isim;
+        int sinir;
+        int tutulan;
+        int tutulan2, tutulan3;
+        string van, tu,tri;
+        public int r1()
+        {
+            Random rnd = new Random();
+            tutulan = rnd.Next(0, sinir);
+            //an = tutulan;
+            return tutulan;
+            
+        }
+        public int r2()
+        {
+            Random rnd2 = new Random();
+            tutulan2 = rnd2.Next(0, sinir);
+            
+            return tutulan2;
+        }
+        public int r3()
+        {
+            Random rnd3 = new Random();
+            tutulan3 = rnd3.Next(1000, 999999);
+            return tutulan3;
+
+        }
+        int isimsayisi = 0;
+        string vo = "", vu = "",ku="";
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sinir = listBox1.Items.Count;
+            for (int i = 0; i < (sinir*sinir-1); i++)
+            {
+                van = "";
+                tu = "";
+                tri = "";
+                van = listBox1.Items[r1()].ToString();
+                tu = listBox1.Items[r2()].ToString();
+                while (van == tu)
+                {
+                    van = listBox1.Items[r1()].ToString();
+                    tu = listBox1.Items[r2()].ToString();
+                }
+                tri = van + " " + tu;
+                listBox2.Items.Add(tri);
+                label2.Text = isimsayisi.ToString();
+                isimsayisi++;
+            }
+            for (int i = 0; i < (sinir * sinir - 1); i++)
+            {
+                vo = "";
+                vu = "";
+                ku = "";
+                vo = listBox1.Items[r1()].ToString();
+                vu = r3().ToString();
+                ku = vo+""+vu;
+                while (textBox1.Text==ku)
+                {
+                    vo = listBox1.Items[r1()].ToString();
+                    vu = r3().ToString();
+                    ku = vo + "" + vu;
+                }
+
+                listBox3.Items.Add(ku);
+                label4.Text = listBox3.Items.Count.ToString();
+                textBox1.Text=ku;
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String[] isimsoyisim = new String[listBox2.Items.Count];
+            String[] kullaniciadi = new String[listBox3.Items.Count];
+
+            listBox2.Items.CopyTo(isimsoyisim, 0);
+            System.IO.File.WriteAllLines("isimvesoyisimler.txt", isimsoyisim);
+
+            listBox3.Items.CopyTo(kullaniciadi, 0);
+            System.IO.File.WriteAllLines("kullaniciadlari.txt", kullaniciadi);
+            System.Diagnostics.Process.Start(Application.StartupPath);
+            MessageBox.Show("İsim ve soyisimler : isimvesoyisimler.txt    Kullanıcı Adları: kullaniciadlari.txt");
+            
+        }
+    }
+}
